26d3a484d7a115d9a3aa98a4ac522dbe3e8c98f4
Johan <johan.mickelin@gmail.com>
2025-01-04 15:35:32 +0100

    top menu design fixes

--- src/components/landing/topMenu.tsx
+++ src/components/landing/topMenu.tsx
@@ -3,15 +3,15 @@ import { Button } from "@/components/ui/button"
 import Link from "next/link"
 import { Separator } from "@/components/ui/separator"
 export const TopMenu = () => (
-  <div className="flex justify-between items-center fixed top-0 left-0 right-0 z-10">
+  <div className="flex justify-between items-center fixed top-0 left-0 right-0 z-[90] mx-4 md:mx-0">
     <div className="flex gap-4 justify-between items-center border max-w-5xl border-muted mx-auto w-full rounded-lg m-2 bg-white shadow-lg">
-      <Logo size="md" />
+      <Logo size="md" className="p-2" />
       <div className="flex gap-4 items-center mr-2">
         <Link href="/jobs">
           <div className="font-normal text-black">Job Board</div>
         </Link>
         <Separator orientation="vertical" />
-        <Link href="#pricing">
+        <Link href="#pricing" className="hidden md:block">
           <div className="font-normal text-black">Pricing</div>
         </Link>
         <Separator orientation="vertical" />
