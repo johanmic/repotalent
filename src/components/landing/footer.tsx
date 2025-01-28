4f227d6c7b5a58db95e70e49934283fd335fc7f7
Johan <johan.mickelin@gmail.com>
2025-01-28 12:55:07 +0100

    footer

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -2,7 +2,7 @@ import { Icon } from "@/components/appIcon"
 import Link from "next/link"
 export function SiteFooter() {
   return (
-    <footer className="border-grid border-t py-6 md:px-8 md:py-0">
+    <footer className="border-grid border-t py-6 md:px-8 md:py-0 text-center">
       <div className="container-wrapper">
         <div className="container py-4">
           <div className="text-balance flex items-center justify-center text-center text-sm leading-loose text-muted-foreground md:text-left">
@@ -27,6 +27,9 @@ export function SiteFooter() {
             <Link href="/legal/privacy">Privacy Policy</Link>
           </div>
         </div>
+      </div>
+      <div className="text-center text-xs leading-loose text-muted-foreground md:text-left flex items-center justify-center gap-2">
+        <a href="https://AIBest.Tools">AI Best Tools</a>
       </div>
     </footer>
   )
